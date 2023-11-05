0dc07d62898b41ff47877fe292c8918c93a308d5
number201724 <number201724@me.com>
2023-11-05 16:31:12 +0800

    Add support for FreeBSD

--- src/zfs-auto-snapshot.sh
+++ src/zfs-auto-snapshot.sh
@@ -227,7 +227,11 @@ do_snapshots () # properties, flags, snapname, oldglob, [targets...]
 if [ "$(uname)" = "Darwin" ]; then
   GETOPT_BIN="$(brew --prefix gnu-getopt 2> /dev/null || echo /usr/local)/bin/getopt"
 else
-  GETOPT_BIN="getopt"
+  if [ "$(uname)" = "FreeBSD" ]; then
+    GETOPT_BIN="/usr/local/bin/getopt"
+  else
+    GETOPT_BIN="getopt"
+  fi
 fi
 
 GETOPT=$($GETOPT_BIN \
